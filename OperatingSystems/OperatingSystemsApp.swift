59ed181019351a0ebec178db5784d3d65ee04e76
Julian Soto <juliansoto04@gmail.com>
2023-09-09 11:35:43 -0600

    Initial Commit

---
+++ OperatingSystems/OperatingSystemsApp.swift
@@ -0,0 +1,17 @@
+//
+//  OperatingSystemsApp.swift
+//  OperatingSystems
+//
+//  Created by Julian Soto on 9/9/23.
+//
+
+import SwiftUI
+
+@main
+struct OperatingSystemsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
